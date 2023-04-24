3e4eefe923af5811dc55989a17e6ce29d5266e4d
d.prigozhaev <d.prigozhaev@netris.ru>
2023-04-24 13:54:19 +0300

    lesson 3: simple CRUD application

---
+++ lesson_3/src/main/java/otus/microservice/architecture/lesson_3/api/common/ResponseBuilder.java
@@ -0,0 +1,29 @@
+package otus.microservice.architecture.lesson_3.api.common;
+
+/**
+ * Utility class for building a JSON application response.
+ *
+ * @author Dmitry Prigozhaev
+ * on 23.10.2022
+ */
+public final class ResponseBuilder {
+
+  /**
+   * Utility class can't be instantiated.
+   */
+  private ResponseBuilder() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  /**
+   * Method for constructing an error response with a reason message
+   * and error code.
+   *
+   * @param code    the status code
+   * @param message the message with the reason for the request failure
+   * @return the {@link ErrorResponse}
+   */
+  public static ErrorResponse error(Integer code, String message) {
+    return new ErrorResponse(code, message);
+  }
+}
